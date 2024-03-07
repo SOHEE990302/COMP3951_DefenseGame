314e688d902da11901f123de0259a477c99a34ae
Sohee Hwang <122145055+SOHEE990302@users.noreply.github.com>
2024-03-06 22:38:35 -0800

    Scene connection

--- Assets/Study/Char_recpg.cs
+++ Assets/Study/Char_recpg.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class NewBehaviourScript : MonoBehaviour
 {
-    // If you click on the screen where the character is
+    //1. If you click on the screen where the character is
     // recognize the character. 
     // pc : If you click the mouse on the screen
     // Mobile: When you touch the screen
 
+    // 정보를 화면에 표시
+    public Text info1;
+    int Input_state;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Input_state = 0;
+        info1.text = "none";
     }
 
     // Update is called once per frame
@@ -20,13 +26,25 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0)==true) //0 <- left / 1,2 <- 중간, 우측
         {
-            print("cliked!");
-            Debug.Log("cliked!");
+            Input_state = 1;
+            //버튼을 눌렀을때 3D 객체를 인식
+            //어떻게 인식하냐 -> 광선 추적
         }
         if(Input.GetMouseButtonUp(0)==true)
         {
-            print("recliked!");
-            Debug.Log("recliked!");
+            Input_state = 2;
+        }
+        ShowInputState(Input_state);
+    }
+    void ShowInputState(int state)
+    {
+        if(state == 1)
+        {
+            info1.text = "눌렀따";
+        }
+        if(state == 2)
+        {
+            info1.text = "떼어졌음";
         }
     }
 }
